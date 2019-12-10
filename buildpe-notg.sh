b70b4254e57ce85e60da1de3e6a3b8dc449cb23b
DaniML3 <46198802+daniml3@users.noreply.github.com>
2019-12-10 19:55:24 +0100

    Create a non-telegram version

---
+++ buildpe-notg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Current CPUs: $(nproc --all)"
+echo "Current ram: $(free -h)"
+clean="no"
+cd ~/android/pixel && repo sync --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+if [ $? -eq 0 ]; then
+else
+exit 1
+fi
+echo "Removing old builds zip files..."
+cd ~/android/pixel
+rm -rf out/target/product/lavender/PixelExperience_lavender-10.0-*
+echo "Done!"
+echo "Initializing build..."
+source build/envsetup.sh
+breakfast lavender
+brunch lavender
+if [ $? -eq 0 ]; then
+    echo "Build completed succesfully! Uploading to /pixel/ folder..."
+    scp  out/target/product/lavender/PixelExperience*-UNOFFICIAL.zip daniml3@frs.sourceforge.net:/home/frs/project/lavenderbuilds/pixel/
+    bash ~/android/jenkins/gen_mirror_json.sh
+    cd  ~/android/ota/ && git add . && git commit -m "Update" && git push
+else
+    echo "Build failed, exiting..."
+fi
