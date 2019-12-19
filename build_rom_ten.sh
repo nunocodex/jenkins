78b0e22b507f6481b10c78cbbf00144fc7f883de
Pereira Ricardo <nunocodex@gmail.com>
2019-12-19 04:16:18 +0100

    Update

--- build_rom_ten.sh
+++ build_rom_ten.sh
@@ -19,6 +19,13 @@ function TG_Maintainers() {
   fi
 }
 
+# TG function to upload file
+function TG_Logs_Upload() {
+  if [ "${TG_BOT_LOG_CHAT}" != "" ]; then
+    curl -s "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendDocument" --data "document=${*}&chat_id=${TG_BOT_LOG_CHAT}&parse_mode=Markdown" > /dev/null
+  fi
+}
+
 function TG_News() {
   if [ "${TG_BOT_NEWS_CHAT}" != "" ]; then
     curl -s "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendmessage" --data "text=*[${CLEAN_BUILD_TYPE}/${jk_device_codename}]:* ${*}&chat_id=${TG_BOT_NEWS_CHAT}&parse_mode=Markdown" > /dev/null
@@ -80,7 +87,7 @@ if [ "$jk_make_clean" = "yes" ]; then
   TG_Log "OUT dir from your repo deleted"
 else
   echo "Removing old builds zip files..."
-  #rm -rf "$BUILD_OUTPUT_DIR"/*"${jk_device_codename}"*
+  rm -rf "$BUILD_OUTPUT_DIR"/*"${jk_device_codename}"*
   echo "Done!"
 fi
 
@@ -88,9 +95,9 @@ echo "Initializing build..."
 TG_Logs "New build started!. Date: ${BUILD_DATETIME}"
 
 # Start building
-#. build/envsetup.sh
-#lunch clean_"${jk_device_codename}"-userdebug
-#make bacon -j$(nproc --all)
+. build/envsetup.sh
+lunch clean_"${jk_device_codename}"-userdebug
+make bacon -j$(nproc --all)
 
 if [ $? -eq 0 ]; then
   MESSAGE="Build completed succesfully!"
@@ -99,8 +106,11 @@ if [ $? -eq 0 ]; then
 
   FILENAME=$(find ${BUILD_OUTPUT_DIR}/CleanDroidOS*.zip | cut -d "/" -f 5)
 
-  bash /home/${SRV_USERNAME}/OTA/gen_mirror_json.sh ${jk_device_codename} ${BUILD_OUTPUT_DIR} v10.0 ${CLEAN_BUILD_TYPE}
+  bash gen_ota_json.sh ${jk_device_codename} ${BUILD_OUTPUT_DIR} "v10.0" ${CLEAN_BUILD_TYPE}
   #cd  /home/${SRV_USERNAME}/OTA/ && git add . && git commit -m "New ${CLEAN_BUILD_TYPE} for ${jk_device_codename} update" && git push
+  TG_Logs_Upload "${BUILD_OUTPUT_DIR}/${FILENAME}.json"
+
+  echo ${BUILD_OUTPUT_DIR}/${FILENAME}.json
 
   #Send new update message.
   TG_Logs "New update detected. Date: ${BUILD_DATETIME}"
---
+++ gen_ota_json.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+CMD_DEVICE_CODENAME=$1
+CMD_DEVICE_OUT=$2
+CMD_CLEAN_VERSION=$3
+CMD_CLEAN_BUILD_TYPE=$4
+
+DATETIME=$(grep "ro.build.date.utc=" ${CMD_DEVICE_OUT}/system/build.prop | cut -d "=" -f 2)
+FILENAME=$(find ${CMD_DEVICE_OUT}/CleanDroidOS-*.zip | cut -d "/" -f 5)
+FILEHASH=$(md5sum ${CMD_DEVICE_OUT}/CleanDroidOS-*.zip | cut -d " " -f 1)
+FILESIZE=$(wc -c ${CMD_DEVICE_OUT}/CleanDroidOS-*.zip | awk '{print $1}')
+DOWNLOAD_URL="https://downloads.sourceforge.net/project/cleandroidos/${CLEAN_BUILD_TYPE}/${CMD_CLEAN_VERSION}/${CMD_DEVICE_CODENAME}/${FILENAME}/download"
+
+msg=$(mktemp)
+{
+  echo -e "{"
+  echo -e "  \x22response\x22: ["
+  echo -e "    {"
+  echo -e "      \x22datetime\x22: ${DATETIME},"
+  echo -e "      \x22filename\x22: \x22${FILENAME}\x22,"
+  echo -e "      \x22id\x22: \x22${FILEHASH}\x22,"
+  echo -e "      \x22size\x22: ${FILESIZE},"
+  echo -e "      \x22romtype\x22: \x22${CMD_CLEAN_BUILD_TYPE}\x22,"
+  echo -e "      \x22url\x22: \x22${DOWNLOAD_URL}\x22,"
+  echo -e "      \x22version\x22: \x22${CMD_CLEAN_VERSION}\x22"
+  echo -e "    }"
+  echo -e "  ]"
+  echo -e "}"
+} > "${msg}"
+
+BJSON=$(cat "${msg}")
+echo "Generate JSON completed"
+echo "${BJSON}" > "${CMD_DEVICE_OUT}/${FILENAME}.json"
