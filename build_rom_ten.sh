1be424ad568f24cefadc44c4e05957e7a95b237a
Pereira Ricardo <nunocodex@gmail.com>
2019-12-19 03:36:38 +0100

    Update

--- build_rom_ten.sh
+++ build_rom_ten.sh
@@ -3,27 +3,25 @@
 # Import custom config
 source $1
 
-TARGET_GAPPS_ARCH=arm64
-
 BUILD_OUTPUT_DIR="out/target/product/${jk_device_codename}"
 
-BUILD_DATETIME=$(date +"%Y%m%d-%H:%m:%s")
+BUILD_DATETIME=$(date +"%Y%m%d-%H:%i")
 
 function TG_Logs() {
   if [ "${TG_BOT_LOG_CHAT}" != "" ]; then
-    curl -s "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendmessage" --data "text=${*}&chat_id=${TG_BOT_LOG_CHAT}&parse_mode=Markdown" > /dev/null
+    curl -s "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendmessage" --data "text=*[${CLEAN_BUILD_TYPE}/${jk_device_codename}]:* ${*}&chat_id=${TG_BOT_LOG_CHAT}&parse_mode=Markdown" > /dev/null
   fi
 }
 
 function TG_Maintainers() {
   if [ "${TG_BOT_MAINTAINERS_CHAT}" != "" ]; then
-    curl -s "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendmessage" --data "text=${*}&chat_id=${TG_BOT_MAINTAINERS_CHAT}&parse_mode=Markdown" > /dev/null
+    curl -s "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendmessage" --data "text=*[${CLEAN_BUILD_TYPE}/${jk_device_codename}]:* ${*}&chat_id=${TG_BOT_MAINTAINERS_CHAT}&parse_mode=Markdown" > /dev/null
   fi
 }
 
 function TG_News() {
   if [ "${TG_BOT_NEWS_CHAT}" != "" ]; then
-    curl -s "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendmessage" --data "text=${*}&chat_id=${TG_BOT_NEWS_CHAT}&parse_mode=Markdown" > /dev/null
+    curl -s "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendmessage" --data "text=*[${CLEAN_BUILD_TYPE}/${jk_device_codename}]:* ${*}&chat_id=${TG_BOT_NEWS_CHAT}&parse_mode=Markdown" > /dev/null
   fi
 }
 
@@ -35,19 +33,19 @@ ccache -M 200G
 
 # Repo clean
 if [ "${jk_repo_clean}" = "yes" ]; then
-  MESSAGE="${CLEAN_BUILD_TYPE}: Repo clean in progress..."
+  MESSAGE="Repo clean in progress..."
   echo $MESSAGE
   TG_Logs $MESSAGE
 
   rm -rf .repo
 
-  MESSAGE="${CLEAN_BUILD_TYPE}: Repo clear, init new repo..."
+  MESSAGE="Repo clear, init new repo..."
   echo $MESSAGE
   TG_Logs $MESSAGE
 
   repo init -u ${REPO_MANIFEST_URL} -b ten
 
-  MESSAGE="${CLEAN_BUILD_TYPE}: Repo init successfully"
+  MESSAGE="Repo init successfully"
   echo $MESSAGE
   TG_Logs $MESSAGE
 fi
@@ -55,7 +53,7 @@ fi
 # Repo sync
 if [ "${jk_repo_sync}" = "yes" ]; then
   echo "Sync started"
-  TG_Logs "Sync started *${CLEAN_BUILD_TYPE}* for *${jk_manifest_branch}* branch"
+  TG_Logs "Sync started for *${jk_manifest_branch}* branch"
 
   SYNC_START=$(date +"%s")
 
@@ -66,10 +64,10 @@ if [ "${jk_repo_sync}" = "yes" ]; then
 
   if [ $? -eq 0 ]; then
     echo "Sync completed successfully"
-    TG_Logs "Sync completed successfully *${CLEAN_BUILD_TYPE}* in *$((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds*"
+    TG_Logs "Sync completed successfully in *$((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds*"
   else
     echo "Sync failed"
-    TG_Logs "Sync failed *${CLEAN_BUILD_TYPE}* in *$((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds*"
+    TG_Logs "Sync failed in *$((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds*"
     exit 1
   fi
 fi
@@ -82,7 +80,7 @@ if [ "$jk_make_clean" = "yes" ]; then
   TG_Log "OUT dir from your repo deleted"
 else
   echo "Removing old builds zip files..."
-  rm -rf "$BUILD_OUTPUT_DIR"/*"${jk_device_codename}"*
+  #rm -rf "$BUILD_OUTPUT_DIR"/*"${jk_device_codename}"*
   echo "Done!"
 fi
 
@@ -90,34 +88,26 @@ echo "Initializing build..."
 TG_Logs "New build started!. Date: ${BUILD_DATETIME}"
 
 # Start building
-. build/envsetup.sh
-lunch clean_"${jk_device_codename}"-userdebug
-make bacon -j$(nproc --all)
+#. build/envsetup.sh
+#lunch clean_"${jk_device_codename}"-userdebug
+#make bacon -j$(nproc --all)
 
 if [ $? -eq 0 ]; then
-  MESSAGE="Build completed succesfully! Uploading..."
+  MESSAGE="Build completed succesfully!"
   echo $MESSAGE
   TG_Logs $MESSAGE
 
-  scp -i /home/${SRV_USERNAME}/.ssh/id_sf ${BUILD_OUTPUT_DIR}/CleanDroidOS*-${CLEAN_BUILD_TYPE}*.zip darkdroiddev@frs.sourceforge.net:/home/frs/p/cleandroidos/${CLEAN_BUILD_TYPE}/
-
-  #echo "Waiting to push OTA."
-  #sleep 15m
-
   FILENAME=$(find ${BUILD_OUTPUT_DIR}/CleanDroidOS*.zip | cut -d "/" -f 5)
-  #echo $FILENAME
 
-  #bash ~/android/jenkins/gen_mirror_json.sh
-  #cd  ~/android/ota/ && git add . && git commit -m "Update" && git push
+  bash /home/${SRV_USERNAME}/OTA/gen_mirror_json.sh ${jk_device_codename} ${BUILD_OUTPUT_DIR} v10.0 ${CLEAN_BUILD_TYPE}
+  #cd  /home/${SRV_USERNAME}/OTA/ && git add . && git commit -m "New ${CLEAN_BUILD_TYPE} for ${jk_device_codename} update" && git push
 
   #Send new update message.
-  TG_Logs "New ${CLEAN_BUILD_TYPE} update detected. Date: ${BUILD_DATETIME}"
+  TG_Logs "New update detected. Date: ${BUILD_DATETIME}"
 
-  UPDATE_URL="https://sourceforge.net/projects/cleandroidos/files/${CLEAN_BUILD_TYPE}/${FILENAME}/download"
+  UPDATE_URL="https://sourceforge.net/projects/cleandroidos/files/${CLEAN_BUILD_TYPE}/v10.0/${jk_device_codename}/${FILENAME}/download"
 
-  TG_Logs "Update ${CLEAN_BUILD_TYPE} url: ${UPDATE_URL}"
-
-  echo "Filename: " $(find ${BUILD_OUTPUT_DIR}/CleanDroidOS*.zip | cut -d "/" -f 5)
+  TG_Logs "Update available in 15 minutes on url: ${UPDATE_URL}"
 else
   MESSAGE="Build of date ${BUILD_DATETIME} failed."
   echo $MESSAGE
